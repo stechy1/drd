e00a43bae71d56aa7261fe3a9bf8c2e664423f36
petr <stechy12@gmail.com>
2017-07-05 11:47:46 +0200

    #15 Vytvoření layoutu okna pro správu kontrolerů pro editaci vlastností nestvůry.

---
+++ src/main/java/cz/stechy/drd/controller/bestiary/edit/BestiaryEditController.java
@@ -0,0 +1,10 @@
+package cz.stechy.drd.controller.bestiary.edit;
+
+import cz.stechy.screens.BaseController;
+
+/**
+ * Kontroler pro řížení kontrolerů pro editaci vlastností nestvůry
+ */
+public class BestiaryEditController extends BaseController {
+
+}
