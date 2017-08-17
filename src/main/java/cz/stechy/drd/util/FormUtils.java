859f89ac7fc81ad8472cac56ba48cd10534300d4
petr <stechy12@gmail.com>
2017-08-17 12:30:16 +0200

    #47 Opravení validátoru číselného vstupu. Nyní je lepší, chytřejší a odolnější.

--- src/main/java/cz/stechy/drd/util/FormUtils.java
+++ src/main/java/cz/stechy/drd/util/FormUtils.java
@@ -2,7 +2,10 @@ package cz.stechy.drd.util;
 
 import cz.stechy.drd.model.MaxActValue;
 import java.util.function.UnaryOperator;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextFormatter.Change;
 import javafx.util.converter.NumberStringConverter;
 
@@ -54,11 +57,8 @@ public final class FormUtils {
     }
 
     public static void initTextFormater(TextField textField, MaxActValue maxActValue) {
-//        textField.setTextFormatter(new TextFormatter<>(new NumberStringConverter(), null,
-//            FormUtils.integerFilter(
-//                maxActValue.getMinValue().intValue(),
-//                maxActValue.getMaxValue().intValue()
-//            )));
+        textField.setTextFormatter(new TextFormatter<>(new NumberStringConverter(),
+            null, new MinMaxUnaryOperator(maxActValue)));
         textField.textProperty()
             .bindBidirectional(maxActValue.actValueProperty(), new NumberStringConverter());
     }
@@ -67,6 +67,74 @@ public final class FormUtils {
         textField.setTextFormatter(null);
         textField.textProperty().unbindBidirectional(maxActValue.actValueProperty());
 
+    }
+
+    public static final class MinMaxUnaryOperator implements UnaryOperator<Change> {
+
+        private final ObjectProperty<Number> min = new SimpleObjectProperty<>();
+        private final ObjectProperty<Number> max = new SimpleObjectProperty<>();
+
+        private MinMaxUnaryOperator(MaxActValue maxActValue) {
+            this.min.bind(maxActValue.minValueProperty());
+            this.max.bind(maxActValue.maxValueProperty());
+        }
+
+        @Override
+        public Change apply(Change change) {
+            String newText = change.getControlNewText();
+            if (newText.matches("-?([1-9][0-9]*|0)?")) {
+                if (newText.trim().isEmpty()) {
+                    return change;
+                }
+
+                // Nedovolím napsat mínus, pokud minimální hodnota je nezáporná
+                if ("-".equals(newText)) {
+                    if (min.get().doubleValue() >= 0) {
+                        return null;
+                    }
+                }
+
+                double value = Double.parseDouble(newText);
+                if (value < min.get().doubleValue() || value > max.get().doubleValue()) {
+                    return null;
+                }
+
+                return change;
+            } else if ("-".equals(change.getText()) ) {
+                // Odebere mínus
+                if (change.getControlText().startsWith("-")) {
+                    if (max.get().doubleValue() < 0) {
+                        return null;
+                    }
+
+                    double value = Double.parseDouble(change.getControlText());
+                    // Pokud je hodnota bez mínusu větší než je max, tak zakážu odebrání mínusu
+                    if (Math.abs(value) > max.get().doubleValue()) {
+                        return null;
+                    }
+
+                    change.setText("");
+                    change.setRange(0, 1);
+                    change.setCaretPosition(change.getCaretPosition()-2);
+                    change.setAnchor(change.getAnchor()-2);
+                    return change;
+                } else {
+                    if (min.get().doubleValue() >= 0) {
+                        return null;
+                    }
+
+                    double value = Double.parseDouble("-" + change.getControlText());
+                    // Pokud je hodnota bez mínusu větší než je max, tak zakážu odebrání mínusu
+                    if (value < min.get().doubleValue()) {
+                        return null;
+                    }
+
+                    change.setRange(0, 0);
+                    return change;
+                }
+            }
+            return null;
+        }
     }
 
 }
