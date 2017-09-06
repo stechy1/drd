b3bd34570444f0c551563e1869d3e9ca581c21ef
petr <stechy12@gmail.com>
2017-09-06 14:00:15 +0200

    #27 Upravení skriptu pro generování zdrojů.

--- scripts/src/resource_generator.py
+++ scripts/src/resource_generator.py
@@ -4,11 +4,21 @@ import os
 
 folder = './src/main/resources/'
 string = 'public static final String {} = "{}";\n'
+constant_builder = 'public static final {} {} = {};\n'
 output_path = './src/main/java/cz/stechy/drd/R.java'
 
-configuration = [
-    'offline_database_name', 'use_online_database', 'online_database_credentials_path', 'hero_levelup_points_per_level'
+constants = [
+	{'name': 'DATABASE_VERSION', 'type': 'int', 'value': '1'}
 ]
+
+configuration = [
+    'offline_database_name', 'use_online_database', 'online_database_credentials_path', 'hero_levelup_points_per_level', 'database_version'
+]
+
+def write_key_pair_values(output_file):
+  for constant in constants:
+    output_file.write("    " + constant_builder.format(constant['type'], constant['name'], constant['value']))
+  output_file.write("\n")
 
 def generate_resources(root_folder, requested_extension, class_name, output_file):
   output_file.write("    public static class " + class_name + " {\n\n")
@@ -42,6 +52,7 @@ with open(output_path, "w", encoding="utf8") as file:
   file.write('@SuppressWarnings("unused")\n')
   file.write("public final class R {\n\n")
 
+  write_key_pair_values(file)
   generate_resources(folder + "fxml", "fxml", "FXML", file)
   generate_translate_keys(folder + "lang/translate_cs_CZ.properties", file)
   generate_config_keys(configuration, file)
