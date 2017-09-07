e9f7332d04974bd6d71872c0c38317c5d8ca1b83
petr <stechy12@gmail.com>
2017-09-07 11:56:39 +0200

    Oprava žádné inicializace batohu. Nevložil se záznam o potomkovi inventáře.

--- src/main/java/cz/stechy/drd/model/inventory/InventoryHelper.java
+++ src/main/java/cz/stechy/drd/model/inventory/InventoryHelper.java
@@ -1,9 +1,11 @@
 package cz.stechy.drd.model.inventory;
 
 import cz.stechy.drd.model.db.DatabaseException;
-import cz.stechy.drd.model.inventory.InventoryRecord.Builder;
+import cz.stechy.drd.model.inventory.InventoryRecord.Metadata;
+import cz.stechy.drd.model.item.Backpack;
 import cz.stechy.drd.model.item.ItemBase;
 import cz.stechy.drd.model.item.ItemRegistry;
+import cz.stechy.drd.model.item.ItemType;
 import cz.stechy.drd.model.persistent.InventoryContent;
 import cz.stechy.drd.model.persistent.InventoryService;
 import java.util.List;
@@ -31,12 +33,27 @@ public class InventoryHelper {
                 try {
                     final Map<Integer, Integer> freeSlotMap = inventoryContent
                         .getFreeSlot(itemBase, itemEntry.getAmmount());
-                    freeSlotMap.entrySet().stream().map(entry -> new Builder()
-                    .inventoryId(inventory.getId())
-                    .itemId(itemEntry.getId())
-                    .ammount(entry.getValue())
-                    .slotId(entry.getKey())
-                    .build()).forEach(record -> {
+                    freeSlotMap.entrySet().stream().map(entry -> {
+                        final Metadata metadata = new Metadata();
+                        if (itemBase.getItemType() == ItemType.BACKPACK) {
+                            assert entry.getValue() == 1;
+                            final Backpack backpack = (Backpack) itemBase;
+                            try {
+                                final String childInventoryId = inventoryManager
+                                    .initSubInventory(backpack.getSize().size);
+                                metadata.put(Backpack.CHILD_INVENTORY_ID, childInventoryId);
+                            } catch (DatabaseException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return new InventoryRecord.Builder()
+                            .inventoryId(inventory.getId())
+                            .itemId(itemEntry.getId())
+                            .ammount(entry.getValue())
+                            .slotId(entry.getKey())
+                            .metadata(metadata)
+                            .build();
+                    }).forEach(record -> {
                         try {
                             inventoryContent.insert(record);
                         } catch (DatabaseException e) {
