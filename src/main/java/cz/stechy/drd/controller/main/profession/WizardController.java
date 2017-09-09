3e363446703d073f41e61b71db341052b0cc5fd1
petr <stechy12@gmail.com>
2017-09-09 12:23:20 +0200

    #71 Přidání informace o kouzelníkově pravděpodobnosti úspěchu a maximální magenergii.

--- src/main/java/cz/stechy/drd/controller/main/profession/WizardController.java
+++ src/main/java/cz/stechy/drd/controller/main/profession/WizardController.java
@@ -1,19 +1,28 @@
 package cz.stechy.drd.controller.main.profession;
 
 import cz.stechy.drd.model.entity.hero.Hero;
+import cz.stechy.drd.model.entity.hero.profession.Wizard;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class WizardController implements IProfessionController, Initializable {
+
+
 
     // region Variables
 
     // region FXML
 
-
+    @FXML
+    private Label lblProbabilityOfSuccess;
 
     // endregion
+
+    private Hero hero;
+    private Wizard wizard;
 
     // endregion
 
@@ -24,6 +33,10 @@ public class WizardController implements IProfessionController, Initializable {
 
     @Override
     public void setHero(Hero hero) {
+        this.hero = hero;
+        this.wizard = new Wizard(hero);
+
+        lblProbabilityOfSuccess.textProperty().bind(wizard.probabilityOfSuccessProperty().asString().concat(" %"));
 
     }
 }
--- src/main/java/cz/stechy/drd/model/entity/hero/profession/Wizard.java
+++ src/main/java/cz/stechy/drd/model/entity/hero/profession/Wizard.java
@@ -1,6 +1,10 @@
 package cz.stechy.drd.model.entity.hero.profession;
 
 import cz.stechy.drd.model.entity.hero.Hero;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
 /**
  * Třída obsahující dovednosti, která dokáže kouzelník na 1. až 5. úrovní
@@ -8,6 +12,9 @@ import cz.stechy.drd.model.entity.hero.Hero;
 public class Wizard {
 
     // region Constants
+
+    private static final int MINIMUM_LEVEL = 1;
+    private static final int MAXIMUM_LEVEL = 5;
 
     private static final int[][] BASIC_MAGENERGY = {
         {7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9}, // 1. úroveň
@@ -25,6 +32,9 @@ public class Wizard {
         32, 32, 47, 47, 47, 61, 61, 74, 74, 85, 85, 95, 95, 99
     };
 
+    private static final int PROBABILITY_OF_SUCCESS_MIN_VALUE = 8;
+    private static final int PROBABILITY_OF_SUCCESS_MAX_VALUE = 21;
+
     // 13 značí, že odvrátit nelze
     private static final int[][] INVISIBLE_MOBS = {
         {11, 11, 13, 13, 13, 13}, // 1. úroveň
@@ -37,6 +47,12 @@ public class Wizard {
     // endregion
 
     // region Variables
+
+    private final IntegerProperty maxMag = new SimpleIntegerProperty(this, "maxMag");
+    private final IntegerProperty probabilityOfSuccess = new SimpleIntegerProperty(this,
+        "probabilityOfSuccess");
+    private final IntegerProperty maxSpellCount = new SimpleIntegerProperty(this,
+        "maxSpellCount");
 
     protected final Hero hero;
 
@@ -51,6 +67,36 @@ public class Wizard {
      */
     public Wizard(Hero hero) {
         this.hero = hero;
+
+        probabilityOfSuccess.bind(Bindings.createIntegerBinding(() -> {
+            int intelligence = hero.getIntelligence().getValue();
+            intelligence = Math.max(PROBABILITY_OF_SUCCESS_MIN_VALUE,
+                Math.min(PROBABILITY_OF_SUCCESS_MAX_VALUE, intelligence));
+            intelligence -= 8;
+            return PROBABILITY_OF_SUCCESS[intelligence];
+        }, hero.getIntelligence().valueProperty()));
+        maxMag.bind(Bindings.createIntegerBinding(() -> {
+            int level = hero.getLevel();
+            int intelligence = hero.getIntelligence().getValue();
+            // Normalizace úrovně a inteligence, abych nevylezl mimo hranice pole
+            level = Math.max(MINIMUM_LEVEL, Math.min(level, MAXIMUM_LEVEL));
+            intelligence = Math.max(PROBABILITY_OF_SUCCESS_MIN_VALUE,
+                Math.min(PROBABILITY_OF_SUCCESS_MAX_VALUE, intelligence));
+            level -= MINIMUM_LEVEL;
+            intelligence -= PROBABILITY_OF_SUCCESS_MIN_VALUE;
+
+            return BASIC_MAGENERGY[level][intelligence];
+        }, hero.levelProperty(), hero.getIntelligence().valueProperty()));
+        maxSpellCount.bind(Bindings.createIntegerBinding(() -> {
+            int level = hero.getLevel();
+            // Normalizace úrovně, abych nevylezl mimo hranici pole
+            level = Math.max(MINIMUM_LEVEL, Math.min(level, MAXIMUM_LEVEL));
+            level -= MINIMUM_LEVEL;
+
+            return MAX_SPELL_COUNT[level];
+        }, hero.levelProperty()));
+
+        hero.getMag().maxValueProperty().bind(maxMag);
     }
 
     // endregion
@@ -58,42 +104,73 @@ public class Wizard {
     // region Public methods
 
     /**
-     * Získá z tabulky maximální počet magů pro zadanou úroveň
+     * Vypočítá pravděpodobnost, že se podaří kouzelníkovi odvrátít neviděného protivníka
+     *
+     * @param typeOfInvisible Typ neviděného protivníka
+     * @return Pravděpodobnost úspěchu
+     */
+    public int getProbabilityOfAvoidInvisibility(int typeOfInvisible) {
+        int level = hero.getLevel();
+        // Normalizace úrovně, abych nevylezl mimo hranici pole
+        level = Math.max(MINIMUM_LEVEL, Math.min(level, MAXIMUM_LEVEL));
+        level -= MINIMUM_LEVEL;
+        final int index = Math.max(0, Math.min(typeOfInvisible, 6));
+
+        return INVISIBLE_MOBS[level][index];
+    }
+
+    // endregion
+
+    // region Getters & Setters
+
+    /**
+     * Získá z tabulky maximální počet magů pro zadanou úroveň.
      * Počet se získá z úrovně a stupně inteligence
      *
      * @return Maximální množství magů pro aktuální úroveň
      */
     public int getMaxMag() {
-        // Normalizace úrovně a obratnosti, abych nevylezl mimo hranice pole
-        final int level = Math.max(1, Math.min(hero.getLevel(), 5));
-        final int intelligence = Math.max(1, Math.min(hero.getIntelligence().getValue() - 8, 14));
+        return maxMag.get();
+    }
 
-        return BASIC_MAGENERGY[level - 1][intelligence - 1];
+    public ReadOnlyIntegerProperty maxMagProperty() {
+        return maxMag;
     }
 
     /**
-     * Získá z tabulky maximální možný počet kouzel na hrdinově úrovni
-     *
-     * @return Maximální možný počet kouzel na hrdinově úrovni
-     */
-    public int getMaxSpellCount() {
-        return MAX_SPELL_COUNT[hero.getLevel() - 1];
-    }
-
-    /**
-     * Spočítá z tabulky pravděpodobnost úspěchu, která je závislá na obratnosti
+     * Vrátí pravděpodobnost úspěchu kouzelníka
      *
      * @return Pravděpodobnost úspěchu
      */
     public int getProbabilityOfSuccess() {
-        return PROBABILITY_OF_SUCCESS[hero.getIntelligence().getValue() - 1];
+        return probabilityOfSuccess.get();
     }
 
-    public int getProbabilityOfAvoidInvisibility(int typeOfInvisible) {
-        final int level = Math.max(1, Math.min(hero.getLevel(), 5));
-        final int index = Math.max(0, Math.min(typeOfInvisible, 6));
+    /**
+     * Vlastnost pravděpodobnost úspěchu
+     *
+     * @return Pravděpodobnost úspěchu
+     */
+    public ReadOnlyIntegerProperty probabilityOfSuccessProperty() {
+        return probabilityOfSuccess;
+    }
 
-        return INVISIBLE_MOBS[level - 1][index];
+    /**
+     * Vrátí maximální možný počet kouzel v závislosti na aktuální úrovni kouzelníka
+     *
+     * @return Maximální počet kouzel
+     */
+    public int getMaxSpellCount() {
+        return maxSpellCount.get();
+    }
+
+    /**
+     * Vlastnost maximální počet kouzel
+     *
+     * @return Maximální počet kouzel
+     */
+    public ReadOnlyIntegerProperty maxSpellCountProperty() {
+        return maxSpellCount;
     }
 
     // endregion
