fda3b4737697d5652f8c825616776cfb26e2b723
petr <stechy12@gmail.com>
2017-09-17 15:13:49 +0200

    #89 Vytvoření grafické reprezentace spojovací čáry mezi dvěma nody.

---
+++ src/main/java/cz/stechy/drd/controller/spellbook/priceeditor/NodeLink.java
@@ -0,0 +1,100 @@
+package cz.stechy.drd.controller.spellbook.priceeditor;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CubicCurve;
+
+/**
+ * Třída reprezentující spojení mezi dvěma nody
+ */
+public class NodeLink extends CubicCurve {
+
+    // region Variables
+
+    private final DoubleProperty mControlOffsetX = new SimpleDoubleProperty();
+    private final DoubleProperty mControlOffsetY = new SimpleDoubleProperty();
+    private final DoubleProperty mControlDirectionX1 = new SimpleDoubleProperty();
+    private final DoubleProperty mControlDirectionY1 = new SimpleDoubleProperty();
+    private final DoubleProperty mControlDirectionX2 = new SimpleDoubleProperty();
+    private final DoubleProperty mControlDirectionY2 = new SimpleDoubleProperty();
+
+    // endregion
+
+    // region Constructors
+
+    /**
+     * Vytvoří nový spoj mezi budoucími dvěma body
+     */
+    public NodeLink() {
+        mControlOffsetX.set(100.0);
+        mControlOffsetY.set(50.0);
+        setStroke(Color.BLACK);
+        setFill(null);
+
+        mControlDirectionX1.bind(Bindings
+            .when(startXProperty().greaterThan(endXProperty()))
+            .then(-1.0)
+            .otherwise(1.0));
+
+        mControlDirectionX2.bind(Bindings
+            .when(startXProperty().greaterThan(endXProperty()))
+            .then(1.0)
+            .otherwise(-1.0));
+
+        controlX1Property().bind(startXProperty()
+            .add(mControlOffsetX.multiply(mControlDirectionX1)));
+
+        controlX2Property().bind(endXProperty()
+            .add(mControlOffsetX.multiply(mControlDirectionX2)));
+
+        controlY1Property().bind(startYProperty()
+            .add(mControlOffsetY.multiply(mControlDirectionY1)));
+
+        controlY2Property().bind(endYProperty()
+            .add(mControlOffsetY.multiply(mControlDirectionY2)));
+    }
+
+    // endregion
+    
+    // region Public methods
+
+    /**
+     * Nastaví startovní pozici linku
+     *
+     * @param startPoint {@link Point2D}
+     */
+    public void setStart(Point2D startPoint) {
+        setStartX(startPoint.getX());
+        setStartY(startPoint.getY());
+    }
+
+    /**
+     * Nastaví koncovou pozici linku
+     *
+     * @param endPoint {@link Point2D}
+     */
+    public void setEnd(Point2D endPoint) {
+        setEndX(endPoint.getX());
+        setEndY(endPoint.getY());
+    }
+
+    /**
+     * Propojí graficky link se dvěma nody
+     *
+     * @param source Zdrojový node
+     * @param target Cílový node
+     */
+    public void bindEnds(Circle source, Circle target) {
+        startXProperty().bind(source.layoutXProperty());
+        startYProperty().bind(source.layoutYProperty());
+        endXProperty().bind(target.layoutXProperty());
+        endYProperty().bind(target.layoutYProperty());
+    }
+
+    // endregion
+
+}
