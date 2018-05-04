f04dc6810377730160a8b54b9d6e5cfcefd50cf0
petr <stechy12@gmail.com>
2018-05-04 12:45:14 +0200

    Menší restrukturalizace tříd.

--- drd_share/src/main/java/cz/stechy/drd/R.java
+++ drd_share/src/main/java/cz/stechy/drd/R.java
@@ -441,7 +441,6 @@ public final class R {
 
         public static class Icon {
 
-            public static final String IMPORT = "/images/icon/import.png";
             public static final String ABOUT = "/images/icon/about.png";
             public static final String ADD = "/images/icon/add.png";
             public static final String BANK = "/images/icon/bank.png";
@@ -461,6 +460,7 @@ public final class R {
             public static final String GENERATE = "/images/icon/generate.png";
             public static final String HAND_CLICK = "/images/icon/hand_click.png";
             public static final String HELP = "/images/icon/help.png";
+            public static final String IMPORT = "/images/icon/import.png";
             public static final String LOGIN = "/images/icon/login.png";
             public static final String MANA = "/images/icon/mana.png";
             public static final String NEW = "/images/icon/new.png";
@@ -485,7 +485,6 @@ public final class R {
         }
 
         public static final String LOGO = "/images/logo.png";
-
         public static class Source {
 
             public static final String LOGIN = "/images/source/login.png";
