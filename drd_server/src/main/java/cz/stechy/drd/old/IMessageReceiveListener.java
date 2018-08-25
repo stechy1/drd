758be8b6b2f91525c59012f7bf8bed6c41ebbabf
petr <stechy12@gmail.com>
2018-08-25 20:55:09 +0200

    Odstranění staré implementace serveru.

--- drd_server/src/main/java/cz/stechy/drd/old/IMessageReceiveListener.java
+++
@@ -1,9 +0,0 @@
-package cz.stechy.drd.old;
-
-import cz.stechy.drd.net.message.IMessage;
-
-public interface IMessageReceiveListener {
-
-    void onMessageReceive(IMessage message, Client client);
-
-}
--- drd_server/src/main/java/cz/stechy/drd/old/ServerInfoProvider.java
+++
@@ -1,8 +0,0 @@
-package cz.stechy.drd.old;
-
-import cz.stechy.drd.net.message.ServerStatusMessage;
-
-@FunctionalInterface
-public interface ServerInfoProvider {
-    ServerStatusMessage getServerStatusMessage();
-}
